8191f45f4ab496d26401ab9c6fbeb1a238a88277
Atharva Tayade <94061850+AtharvaTayade2005@users.noreply.github.com>
2026-06-19 17:02:28 +0530

    adding email alerts

--- android/app/src/main/java/com/saumi/aivala/MainActivity.java
+++ android/app/src/main/java/com/saumi/aivala/MainActivity.java
@@ -1,9 +1,6 @@
 package com.saumi.aivala;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,17 +8,6 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Reset all aggressive transparency - we are going back to basics
-        if (bridge != null) {
-            WebView webView = bridge.getWebView();
-            webView.setBackgroundColor(Color.WHITE);
-        }
-        getWindow().setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(Color.BLACK));
     }
 
 }
